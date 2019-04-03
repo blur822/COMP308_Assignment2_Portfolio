215471a8dcf1bd3998487ca65f2ae2553c14fca2
Atul Sharma <sharmaboyz77@gmail.com>
2019-04-03 17:47:55 -0400

    modifying css

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,37 +1,45 @@
 // Modules
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 // Components
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './partials/header/header.component';
-import { FooterComponent } from './partials/footer/footer.component';
-import { BasePageComponent } from './partials/base-page/base-page.component';
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
-import { ContactListComponent } from './contacts/contact-list/contact-list.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { LoginComponent } from './pages/login/login.component';
-import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
-import { ContactDeleteComponent } from './contacts/contact-delete/contact-delete.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./partials/header/header.component";
+import { FooterComponent } from "./partials/footer/footer.component";
+import { BasePageComponent } from "./partials/base-page/base-page.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from "./pages/about/about.component";
+
+import { ServicesComponent } from "./pages/services/services.component";
+import { ContactComponent } from "./pages/contact/contact.component";
+import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
+import { ContactListComponent } from "./contacts/contact-list/contact-list.component";
+import { RegisterComponent } from "./pages/register/register.component";
+import { LoginComponent } from "./pages/login/login.component";
+import { ContactDetailsComponent } from "./contacts/contact-details/contact-details.component";
+import { ContactDeleteComponent } from "./contacts/contact-delete/contact-delete.component";
 
 // Services
-import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
-import { AuthService } from './services/auth.service';
-import { JwtModule, JwtHelperService, JwtInterceptor } from '@auth0/angular-jwt';
+import {
+  FlashMessagesModule,
+  FlashMessagesService
+} from "angular2-flash-messages";
+import { AuthService } from "./services/auth.service";
+import {
+  JwtModule,
+  JwtHelperService,
+  JwtInterceptor
+} from "@auth0/angular-jwt";
 
 // Route Guards
-import { AuthGuard } from './guards/auth.guard';
+import { AuthGuard } from "./guards/auth.guard";
+import { ProjectsComponent } from "./pages/projects/projects.component";
 
 export function jwtTokenGetter() {
-  return localStorage.getItem('id_token');
+  return localStorage.getItem("id_token");
 }
 
 @NgModule({
@@ -42,7 +50,7 @@ export function jwtTokenGetter() {
     BasePageComponent,
     HomeComponent,
     AboutComponent,
-    ProductsComponent,
+    ProjectsComponent,
     ServicesComponent,
     ContactComponent,
     PageNotFoundComponent,
@@ -67,4 +75,4 @@ export function jwtTokenGetter() {
   providers: [FlashMessagesService, AuthGuard],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-   //"URI" : "mongodb://localhost/test",
-    "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
-    "secret": "SomeSecret"
-}
+  URI: "mongodb://localhost/demo",
+  // "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
+  secret: "SomeSecret"
+};
