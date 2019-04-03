72bac44f9a799dcaed712ddaaa2d2fcbda485ce4
Atul Sharma <sharmaboyz77@gmail.com>
2019-04-03 18:56:43 -0400

    fixing

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
-  URI: "mongodb://localhost/demo",
+  // URI: "mongodb://localhost/demo",
+  "URI": "mongodb://atul:a123456@ds018258.mlab.com:18258/atulsharma",
   // "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
   secret: "SomeSecret"
 };
